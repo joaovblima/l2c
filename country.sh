1fe50264713f83c7d79f48e99cb15739cf69215b
João Lima <joaovictorbernardinodelima@gmail.com>
2026-02-20 16:54:56 -0300

    adiciona exemplo com variaveis e entrada de dados

---
+++ country.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+country=brasil
+same_country=$country
+
+echo $country
+echo $same_country
---
+++ entered_name.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "whats yout name?"
+read entered_name
+
+echo -e "\nplease, welcome my friend" $entered_name
